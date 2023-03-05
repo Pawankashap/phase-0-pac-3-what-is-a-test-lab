66434da0a3a6a6f5de764aa275def410ba01a371
Fnu Pawan Kumar <pawan.3940@gmail.com>
2023-03-05 14:45:10 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const name = "Joe";
-const height = 74;
+const name = "Susan";  //Nmae change Joe to Sussan
+const height = 39;   // Height decrease 72 to 39
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
